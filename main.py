d143573efa4acd1d4ffb2ae9fd6dddc4de1e7807
Фёдор Горленко <fedorgorlenko@gmail.com>
2024-05-21 18:47:21 +0300

    WEB update

--- main.py
+++ main.py
@@ -1,80 +1,50 @@
-from fpdf import FPDF
-from PIL import Image
-from math import ceil
-from os import listdir
+from flask import Flask, render_template, request, redirect, abort, flash
+import os
+from utils.img2pdf import img2pdf
+from uuid import uuid4
+from random import choice
 
-#max a4 size: 210,297
-#1px=0.264583 mm
-
-PAGE_SIZE_Y = 297 # размер страници
-PAGE_SIZE_X = 210
-PIXEL_SIZE = 0.264583 # размер пикселя в мм
-SHAKAL = 0.5 # Степень сжатия изображения
-OTSTUP_PX = 5
-
-img_on_pages = [] #Размещение картинок на страницах
-img_sizes = [] # размеры картинок в mm
-allowed_types = ['jpg', 'jpeg', 'png'] # Разрешённые типы
+app = Flask(__name__)
 
 
-def px_to_mm(px): # преобразование пикселей в милиметры
-    return ceil(px * PIXEL_SIZE * SHAKAL)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-def get_shakal_coef(x):
-    return (PAGE_SIZE_X - OTSTUP_PX * 2) / px_to_mm(x)
+@app.route('/var')
+def select_class():
+    return render_template('select_class.html', classes=os.listdir(f'./questions'))
 
 
-def get_sizes(): # Получение размеров изоюражений из папки images
-    for i in listdir('./images'):
-        if i.split('.')[-1] in allowed_types:
-            im = Image.open(f'./images/{i}')
-            width, height = im.size
-            if px_to_mm(height) > PAGE_SIZE_Y - OTSTUP_PX * 2:
-                print(ceil(height * PIXEL_SIZE))
-                continue
-            img_sizes.append((i, height, width))
-            im.close()
-    img_sizes.sort(reverse=True, key=lambda x: x[1])
+@app.route('/var/<class_>')
+def get_class(class_):
+    return render_template('select_predmet.html', predmets=os.listdir(f'./questions/{class_}'), cl=class_)
 
 
-def make_page(): # Создание удоборимой страници
-    images_size = OTSTUP_PX
-    ans = []
-    while images_size < PAGE_SIZE_Y - OTSTUP_PX and len(img_sizes):
-        for i in range(len(img_sizes)):
-            if images_size + px_to_mm(img_sizes[i][1]) <= PAGE_SIZE_Y - OTSTUP_PX:
-                images_size += px_to_mm(img_sizes[i][1])
-                ans.append(img_sizes.pop(i))
-                break
-            if i == len(img_sizes):
-                return ans
-            if images_size + px_to_mm(img_sizes[-1][1]) > PAGE_SIZE_Y - OTSTUP_PX:
-                return ans
-    return ans
+@app.route('/var/<class_>/<predmet>')
+def generate(class_, predmet):
+    zadanija = []
+    ansvers = []
+    variant_id = uuid4()
+    for i in os.listdir(f'./questions/{class_}/{predmet}'):
+        a = choice(os.listdir(f'./questions/{class_}/{predmet}/{i}'))
+        zadanija.append(f'./questions/{class_}/{predmet}/{i}/{a}')
+        ansvers.append(a)
+    with open(f'./static/variants/answers/{variant_id}.txt', 'w') as f:
+        f.writelines(list(map(lambda x: x.split('_')[0], ansvers)))
+    img2pdf(f'./static/variants/pdfs/{variant_id}', zadanija)
+    return redirect(f'/get_var/{variant_id}')
 
 
-def make_pages(): # Планировка страниц
-    get_sizes()
-    while len(img_sizes) != 0:
-        page = make_page()
-        if page:
-            img_on_pages.append(page)
+@app.route('/get_var/<variant>')
+def get_var(variant):
+    return render_template('get_var.html', variant=variant)
 
 
- 
-def run(): # Отрисовка страниц
-    pdf = FPDF()
-    make_pages()
-    for i in img_on_pages:
-        print(i)
-        pdf.add_page()
-        pos =  OTSTUP_PX
-        for j in i:
-            pdf.image(f'./images/{j[0]}', x=OTSTUP_PX, y=pos, w=px_to_mm(j[2]) * get_shakal_coef(j[2]), h=px_to_mm(j[1]) * get_shakal_coef(j[2]))
-            pos = pos + px_to_mm(j[1]) * get_shakal_coef(j[2])
-    pdf.output("images.pdf")
+@app.route('/select_var')
+def select_var():
+    return render_template('select_var.html', variants=[i[:-4] for i in os.listdir('./static/variants/pdfs')])
 
 
-if __name__ == '__main__':
-    run()
+app.run()
---
+++ utils/img2pdf.py
@@ -0,0 +1,78 @@
+from fpdf import FPDF
+from PIL import Image
+from math import ceil
+from os import listdir
+
+#max a4 size: 210,297
+#1px=0.264583 mm
+
+PAGE_SIZE_Y = 297 # размер страници
+PAGE_SIZE_X = 210
+PIXEL_SIZE = 0.264583 # размер пикселя в мм
+SHAKAL = 0.5 # Степень сжатия изображения
+OTSTUP_PX = 5
+
+img_on_pages = [] #Размещение картинок на страницах
+img_sizes = [] # размеры картинок в mm
+allowed_types = ['jpg', 'jpeg', 'png'] # Разрешённые типы
+
+
+def px_to_mm(px): # преобразование пикселей в милиметры
+    return ceil(px * PIXEL_SIZE * SHAKAL)
+
+
+def get_shakal_coef(x):
+    return (PAGE_SIZE_X - OTSTUP_PX * 2) / px_to_mm(x)
+
+
+def get_sizes(image_paths: list): # Получение размеров изоюражений из папки images
+    for i in image_paths:
+        print(i.split('.')[-1])
+        if i.split('.')[-1] in allowed_types:
+            im = Image.open(i)
+            width, height = im.size
+            if px_to_mm(height) > PAGE_SIZE_Y - OTSTUP_PX * 2:
+                print(ceil(height * PIXEL_SIZE))
+                continue
+            img_sizes.append((i, height * get_shakal_coef(width), width * get_shakal_coef(width)))
+            im.close()
+    img_sizes.sort(reverse=True, key=lambda x: x[1])
+
+
+def make_page(): # Создание удоборимой страници
+    images_size = OTSTUP_PX
+    ans = []
+    while images_size < PAGE_SIZE_Y - OTSTUP_PX and len(img_sizes):
+        for i in range(len(img_sizes)):
+            if images_size + px_to_mm(img_sizes[i][1]) <= PAGE_SIZE_Y - OTSTUP_PX:
+                images_size += px_to_mm(img_sizes[i][1])
+                ans.append(img_sizes.pop(i))
+                break
+            if i == len(img_sizes):
+                return ans
+            if images_size + px_to_mm(img_sizes[-1][1]) > PAGE_SIZE_Y - OTSTUP_PX:
+                return ans
+    return ans
+
+
+def make_pages(image_paths: list): # Планировка страниц
+    get_sizes(image_paths)
+    while len(img_sizes) != 0:
+        page = make_page()
+        if page:
+            img_on_pages.append(page)
+
+
+ 
+def img2pdf(pdf_name: str, image_paths: list): # Отрисовка страниц
+    pdf = FPDF()
+    make_pages(image_paths)
+    print(img_sizes)
+    for i in img_on_pages:
+        print(i)
+        pdf.add_page()
+        pos =  OTSTUP_PX
+        for j in i:
+            pdf.image(f'{j[0]}', x=OTSTUP_PX, y=pos, w=px_to_mm(j[2]), h=px_to_mm(j[1]))
+            pos = pos + px_to_mm(j[1]) * get_shakal_coef(j[2])
+    pdf.output(f"{pdf_name}.pdf")
