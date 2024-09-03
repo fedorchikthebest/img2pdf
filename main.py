3c98421984b8831b517dffc074c0771ed28ad1f1
user <f@f.f>
2024-09-03 13:22:41 +0300

    bug fix an disain

--- main.py
+++ main.py
@@ -14,19 +14,22 @@ def fshuffle(arr:list) -> list:
     return arr
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == 'POST':
+        var = request.form.get('var')
+        return render_template('get_var.html', variant=var)
     return render_template('index.html')
 
 
 @app.route('/var')
 def select_predmet():
-    return render_template('select_class.html', classes=list(filter(lambda x: x != ".gitkeep", os.listdir(f'./questions'))))
+    return render_template('select_class.html', classes=list(filter(lambda x: x[0] != "." and x != 'var.bin', os.listdir(f'./questions'))))
 
 
 @app.route('/var/<predmet>')
 def get_class(predmet):
-    return render_template('select_predmet.html', predmets=os.listdir(f'./questions/{predmet}'), cl=predmet)
+    return render_template('select_predmet.html', predmets=list(filter(lambda x: x[0] != "." and x != 'var.bin', os.listdir(f'./questions/{predmet}'))), cl=predmet)
 
 
 @app.route('/var/<predmet>/<class_>', methods=["GET", "POST"])
@@ -34,28 +37,35 @@ def generate(predmet, class_):
     if request.method == "GET":
         questions = []
         for i in os.listdir(f'{os.getcwd()}/questions/{predmet}/{class_}'):
-            questions.append((i, len(os.listdir(f'{os.getcwd()}/questions/{predmet}/{class_}/{i}'))))
+            if i != '.DS_Store':
+                questions.append((i, len(os.listdir(f'{os.getcwd()}/questions/{predmet}/{class_}/{i}'))))
+                questions.sort()
         return render_template("select_questions.html", questions=questions)
     zadanija = []
     ansvers = []
     variant_id = 0
     try:
         with open(f'{os.getcwd()}/questions/var.bin', 'rb') as f:
-            variant_id = int.from_bytes(f.read()) + 1
+            variant_id = int.from_bytes(f.read(), "big") + 1
         with open(f'{os.getcwd()}/questions/var.bin', 'wb') as f:
-            f.write(variant_id.to_bytes(8))
+            f.write(variant_id.to_bytes(8, "big"))
     except FileNotFoundError:
         with open(f'{os.getcwd()}/questions/var.bin', 'wb') as f:
-            f.write(variant_id.to_bytes(8))
+            f.write(variant_id.to_bytes(8, "big"))
+    cnt = 0
     for i in request.form.keys():
-        if i != '.DS_Storage':
+        if i[0] != '.':
             a = fshuffle(os.listdir(f'{os.getcwd()}/questions/{predmet}/{class_}/{i}'))[:int(request.form.get(i))]
             for j in a:
-                zadanija.append(f'{os.getcwd()}/questions/{predmet}/{class_}/{i}/{j}')
-        ansvers += a
+                if j[0] != '.':
+                    zadanija.append(f'{os.getcwd()}/questions/{predmet}/{class_}/{i}/{j}')
+
+    ansvers = img2pdf(f'{os.getcwd()}/static/variants/pdfs/{variant_id}', zadanija)
+    for i in range(len(ansvers)):
+        ansvers[i] = f'{i + 1}) {ansvers[i][:ansvers[i].rfind(".")].capitalize()}'
     with open(f'{os.getcwd()}/static/variants/answers/{variant_id}.txt', 'w') as f:
+        f.write(f'Вариант № {variant_id}' + '\n')
         f.write('\n'.join(list(map(lambda x: x.split('_')[0], ansvers))))
-    img2pdf(f'{os.getcwd()}/static/variants/pdfs/{variant_id}', zadanija)
     return redirect(f'/get_var/{variant_id}')
 
 
@@ -66,7 +76,7 @@ def get_var(variant):
 
 @app.route('/select_var')
 def select_var():
-    return render_template('select_var.html', variants=[i[:-4] for i in os.listdir('./static/variants/pdfs')])
+    return render_template('select_var.html', variants=[i[:-4] for i in os.listdir('./static/variants/pdfs') if i[0] != '.'])
 
 
 app.run()
--- utils/img2pdf.py
+++ utils/img2pdf.py
@@ -1,3 +1,5 @@
+import os.path
+
 from fpdf import FPDF
 from PIL import Image
 from math import ceil
@@ -39,7 +41,6 @@ def get_shakal_coef(x):
 
 def get_sizes(image_paths: list): # Получение размеров изоюражений из папки images
     for i in image_paths:
-        print(i.split('.')[-1])
         if i.split('.')[-1] in allowed_types:
             im = Image.open(i)
             width, height = im.size
@@ -51,12 +52,10 @@ def get_sizes(image_paths: list): # Получение размеров изою
     img_sizes.sort(reverse=True, key=lambda x: x[1])
 
 
-def make_page(): # Создание удоборимой страници
-    current_number = 1
+def make_page(current_number): # Создание удоборимой страници
     images_size = OTSTUP_PX + TEXT_HEIGHT
     ans = [(f"text:Вариант №{VARIANT}", OTSTUP_PX, ZATICHKA_WEIGHT)]
     while images_size < PAGE_SIZE_Y - OTSTUP_PX and len(img_sizes):
-        print(1)
         for i in range(len(img_sizes)):
             if images_size + px_to_mm(img_sizes[i][1]) + TEXT_HEIGHT + TEXT_HEIGHT <= PAGE_SIZE_Y - OTSTUP_PX:
                 images_size += px_to_mm(img_sizes[i][1]) + TEXT_HEIGHT + TEXT_HEIGHT
@@ -66,16 +65,17 @@ def make_page(): # Создание удоборимой страници
                 current_number += 1
                 break
             if i == len(img_sizes):
-                return ans
+                return ans, current_number
             if images_size + px_to_mm(img_sizes[-1][1]) + TEXT_HEIGHT + TEXT_HEIGHT > PAGE_SIZE_Y - OTSTUP_PX:
-                return ans
-    return ans
+                return ans, current_number
+    return ans, current_number
 
 
 def make_pages(image_paths: list): # Планировка страниц
     get_sizes(image_paths)
+    current_number = 1
     while len(img_sizes) != 0:
-        page = make_page()
+        page, current_number = make_page(current_number)
         if page:
             img_on_pages.append(page)
 
@@ -87,11 +87,10 @@ def img2pdf(pdf_name: str, image_paths: list): # Отрисовка страни
     pdf = FPDF()
     pdf.add_font("Rus", fname=FONT_PATH, style="", uni=True)
     make_pages(image_paths)
-    print(img_sizes)
+    answers = []
     for i in img_on_pages:
-        print(i)
         pdf.add_page()
-        pos =  OTSTUP_PX
+        pos = OTSTUP_PX
         pdf.set_font("Rus", "", TEXT_HEIGHT)
         for j in i:
             if j[0].split(':')[0] == "text":
@@ -99,7 +98,9 @@ def img2pdf(pdf_name: str, image_paths: list): # Отрисовка страни
                 pos = pos + TEXT_HEIGHT
             else:
                 pdf.image(f'{j[0]}', x=OTSTUP_PX, y=pos, w=px_to_mm(j[2]), h=px_to_mm(j[1]))
+                answers.append(os.path.split(j[0])[-1])
                 pos = pos + px_to_mm(j[1])
     pdf.output(f"{pdf_name}.pdf")
     img_on_pages.clear()
     img_sizes.clear()
+    return answers
